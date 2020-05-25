d9aae3eb549a7be648bdda49d66b5c4760e6f05e
Askar Khabibullin <askar.master@yandex.ru>
2020-05-25 13:31:08 +0500

    Updates

---
+++ precache-manifest.5bf8a9d6a3942915a92130e8488b883e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9edcffe8b100a6537b5b58a2ee0a409",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "88d1cffceb44506f9387",
+    "url": "/monsters-rolodex/static/css/main.ab391005.chunk.css"
+  },
+  {
+    "revision": "abd7d71b0a1f3859dfb8",
+    "url": "/monsters-rolodex/static/js/2.0adb66df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.0adb66df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88d1cffceb44506f9387",
+    "url": "/monsters-rolodex/static/js/main.86223661.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
